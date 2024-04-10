1513ee22b2940e364d8653cf1c1fac9b22f7adc1
Ruralpainter857 <166540945+Ruralpainter857@users.noreply.github.com>
2024-04-10 16:51:47 +0200

    bouh

--- src/main/java/fr/yteriagrp/ytermod/init/YtermodModTabs.java
+++ src/main/java/fr/yteriagrp/ytermod/init/YtermodModTabs.java
@@ -4,6 +4,7 @@
  */
 package fr.yteriagrp.ytermod.init;
 
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.resources.ResourceLocation;
@@ -18,10 +19,10 @@ import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import fr.yteriagrp.ytermod.YtermodMod;
 
 public class YtermodModTabs {
-	public static ResourceKey<CreativeModeTab> TAB_YTERMOD_ORES = ResourceKey.create(Registries.CREATIVE_MODE_TAB, new ResourceLocation(YtermodMod.MODID, "ytermod_ores"));
+	public static ResourceKey<CreativeModeTab> TAB_YTERMODORES = ResourceKey.create(Registries.CREATIVE_MODE_TAB, new ResourceLocation(YtermodMod.MODID, "ytermodores"));
 
 	public static void load() {
-		Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB, TAB_YTERMOD_ORES,
-				FabricItemGroup.builder().title(Component.translatable("item_group." + YtermodMod.MODID + ".ytermod_ores")).icon(() -> new ItemStack(YtermodModItems.CUPPER_DIAM_POWDER)).build());
+		Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB, TAB_YTERMODORES,
+				FabricItemGroup.builder().title(Component.translatable("item_group." + YtermodMod.MODID + ".ytermodores")).icon(() -> new ItemStack(Blocks.POLISHED_BLACKSTONE_SLAB)).build());
 	}
 }
